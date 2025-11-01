8cc1c74471eafad8150bbd03c78584b198bfb5c0
[dyad] <imagineiasolucoes@gmail.com>
2025-11-01 01:21:46 -0300

    [dyad] wrote 1 file(s)

---
+++ src/components/backup/SimpleBackupTest.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SimpleBackupTest: React.FC = () => {
+  return (
+    <div className="bg-green-500 text-white p-4 rounded-md text-center font-semibold">
+      Teste de Backup - OK
+    </div>
+  );
+};
+
+export default SimpleBackupTest;
